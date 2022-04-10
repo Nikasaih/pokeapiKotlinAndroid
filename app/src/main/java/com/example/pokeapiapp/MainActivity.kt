93dfbf2fd9e9a0b10cdc4b1a8d11f2eb302e1605
Nikasaih <gchaducnguyen@gmail.com>
2022-04-10 20:15:34 +0200

    add some code

--- app/src/main/java/com/example/pokeapiapp/MainActivity.kt
+++ app/src/main/java/com/example/pokeapiapp/MainActivity.kt
@@ -6,29 +6,36 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pokeapiapp.services.initializeTypeMap
 import com.example.pokeapiapp.services.queryPokemon
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        initializeTypeMap()
         setContentView(R.layout.activity_main)
 
         try {
-
             changePokemon()
-          val pokemonFound = queryPokemon("lucario")
-            //val pokemonImg = findViewById<ImageView>(R.id.pokemonImg)
-findViewById<TextView>(R.id.heightText).setText("hbjbbbuh")
-            //val imgUri = Uri.parse(pokemonFound.imgSrc)
-           // pokemonImg.setImageURI(imgUri)
-
         } catch (e: Exception) {
             println(e)
         }
     }
 
-    fun changePokemon() {
+    private fun changePokemon() {
+        val pokemonFound = queryPokemon("lucario")
+        val pokemonImg = findViewById<ImageView>(R.id.pokemonImg)
+        findViewById<TextView>(R.id.heightText).setText("hbjbbbuh")
+        val imgUri = Uri.parse(pokemonFound.imgSrc)
+        val imgType = findViewById<ImageView>(R.id.pokemonType1)
+        val imgTypeUri = Uri.parse(pokemonFound.types[0])
+        imgType.setImageURI(imgTypeUri)
 
+        if (pokemonFound.types.size > 1) {
+            val imgType2 = findViewById<ImageView>(R.id.pokemonType2)
+            val imgTypeUri2 = Uri.parse(pokemonFound.types[1])
+            imgType2.setImageURI(imgTypeUri2)
+        }
+        pokemonImg.setImageURI(imgUri)
     }
 }
--- app/src/main/java/com/example/pokeapiapp/services/PokemonServices.kt
+++ app/src/main/java/com/example/pokeapiapp/services/PokemonServices.kt
@@ -6,6 +6,29 @@ import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 
+val typesMap: MutableMap<String, String> = mutableMapOf()
+
+fun initializeTypeMap(){
+    typesMap.put("bug","https://www.pokepedia.fr/images/thumb/e/ee/Miniature_Type_Insecte_EB.png/68px-Miniature_Type_Insecte_EB.png")
+    typesMap.put("steel","https://www.pokepedia.fr/images/thumb/b/b9/Miniature_Type_Acier_EB.png/68px-Miniature_Type_Acier_EB.png")
+    typesMap.put("fire","https://www.pokepedia.fr/images/thumb/f/fc/Miniature_Type_Feu_EB.png/68px-Miniature_Type_Feu_EB.png")
+    typesMap.put("grass","https://www.pokepedia.fr/images/thumb/3/35/Miniature_Type_Plante_EB.png/68px-Miniature_Type_Plante_EB.png")
+    typesMap.put("electric","https://www.pokepedia.fr/images/thumb/6/6c/Miniature_Type_%C3%89lectrik_EB.png/68px-Miniature_Type_%C3%89lectrik_EB.png")
+    typesMap.put("psychic","https://www.pokepedia.fr/images/thumb/d/da/Miniature_Type_Psy_EB.png/68px-Miniature_Type_Psy_EB.png")
+    typesMap.put("dragon","https://www.pokepedia.fr/images/thumb/2/23/Miniature_Type_Dragon_EB.png/68px-Miniature_Type_Dragon_EB.png")
+    typesMap.put("dark","https://www.pokepedia.fr/images/thumb/f/f4/Miniature_Type_T%C3%A9n%C3%A8bres_EB.png/68px-Miniature_Type_T%C3%A9n%C3%A8bres_EB.png")
+    typesMap.put("ground","https://www.pokepedia.fr/images/thumb/d/d6/Miniature_Type_Sol_EB.png/68px-Miniature_Type_Sol_EB.png")
+    typesMap.put("normal","https://www.pokepedia.fr/images/thumb/2/2e/Miniature_Type_Normal_EB.png/68px-Miniature_Type_Normal_EB.png")
+    typesMap.put("fighting","https://www.pokepedia.fr/images/thumb/1/1c/Miniature_Type_Combat_EB.png/68px-Miniature_Type_Combat_EB.png")
+    typesMap.put("flying","https://www.pokepedia.fr/images/thumb/6/62/Miniature_Type_Vol_EB.png/68px-Miniature_Type_Vol_EB.png")
+    typesMap.put("poison","https://www.pokepedia.fr/images/thumb/2/28/Miniature_Type_Poison_EB.png/68px-Miniature_Type_Poison_EB.png")
+    typesMap.put("rock","https://www.pokepedia.fr/images/thumb/d/d3/Miniature_Type_Roche_EB.png/68px-Miniature_Type_Roche_EB.png")
+    typesMap.put("ghost","https://www.pokepedia.fr/images/thumb/e/e5/Miniature_Type_Spectre_EB.png/68px-Miniature_Type_Spectre_EB.png")
+    typesMap.put("water","https://www.pokepedia.fr/images/thumb/4/4c/Miniature_Type_Eau_EB.png/68px-Miniature_Type_Eau_EB.png")
+    typesMap.put("ice","https://www.pokepedia.fr/images/thumb/7/7e/Miniature_Type_Glace_EB.png/68px-Miniature_Type_Glace_EB.png")
+    typesMap.put("fairy","https://www.pokepedia.fr/images/thumb/3/3e/Miniature_Type_F%C3%A9e_EB.png/68px-Miniature_Type_F%C3%A9e_EB.png")
+}
+
 fun queryPokemon(pokemonName: String): PokemonData {
     val searchResponse: MutableMap<String, String> = searchPokemon(pokemonName)
     val jsonResponse: String = searchResponse.get("jsonResponse").orEmpty()
